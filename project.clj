7316572b53f2e2fd353b1e7ba38be0f0f5436ade
Gonçalo Garcia <goncalo.torrao@talkdesk.com>
2020-07-18 23:00:03 +0100

    Add clojurescript support

--- project.clj
+++ project.clj
@@ -5,8 +5,11 @@
             :url  "none"
             :year 2020
             :key "mpl-2.0"}
+  ;; CLJ AND CLJS source code paths
+  :source-paths ["src/clj" "src/cljs"]
   :dependencies [
                  [org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.773"]
                  [clj-http "3.10.1"]
                  [org.clojure/data.xml "0.0.8"]
                  ]
@@ -14,7 +17,24 @@
             [lein-cljfmt "0.6.8"]
             [lein-cloverage "1.1.2"]
             [lein-license "0.1.8"]
+            [lein-cljsbuild "1.1.8"]
             ]
+  ;; cljsbuild options configuration
+  :cljsbuild {:builds
+              [{;; CLJS source code path
+                :source-paths ["src/cljs"]
+
+                ;; Google Closure (CLS) options configuration
+                :compiler {;; CLS generated JS script filename
+                           :output-to "resources/public/js/modern.js"
+
+                           ;; minimal JS optimization directive
+                           :optimizations :whitespace
+
+                           ;; generated JS code prettyfication
+                           :pretty-print true}}]}
+  ;; to clean JS files generated during the build
+  :clean-targets ^{:protect false} [:target-path "resources/public/js/"]
   :main ^:skip-aot goodstats.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
---
+++ src/cljs/goodstats/index.cljs
@@ -0,0 +1,5 @@
+(ns goodstats.index
+  (:require [clojure.browser.repl :as repl]))
+
+(repl/connect "http://localhost:9000/repl")
+
